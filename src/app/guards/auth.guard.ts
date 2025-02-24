bee27814a88e1cda02cd2d7ac634459aa54550c5
BAthiphong <Athiphong.bunkhu@gmail.com>
2025-02-24 18:24:26 +0700

    :sparkle: add auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -7,15 +7,6 @@ export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router)
   const userService = inject(UserService)
 
-  // const userData = userService.getUserData();
-  // if(userData) {
-  //   return true;
-  // }
-  // else {
-  //   router.navigateByUrl("login");
-  //   return false;
-  // }
-
   const localData = localStorage.getItem("token")
   if(localData != null){
     return true;
